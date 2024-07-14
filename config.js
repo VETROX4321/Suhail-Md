059d7f91eaaa1130025fcff3c66b1f48f727662a
VETROX4321 <kousikr3355@gmail.com>
2024-07-14 19:57:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -91,7 +91,7 @@ module.exports = {
 
 
   WORKTYPE: process.env.WORKTYPE || process.env.MODE|| "private",
-  LANG: ( process.env.THEME ||  "KOUSHIK"  ).toUpperCase(),
+  LANG: ( process.env.THEME ||  "SUHAIL"  ).toUpperCase(),
 
 
 
